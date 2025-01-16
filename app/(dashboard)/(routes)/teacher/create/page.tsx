cd0657b5711a6e670c8d56af7d4034d39de50e37
Gopi I <igopi170@gmail.com>
2025-01-16 23:58:29 +0530

    prisma + postgres connected and created course

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -38,8 +38,9 @@ const CreatePage = () => {
     const {isSubmitting , isValid} = form.formState;
     const onSubmit = async (values : z.infer<typeof formSchema>) => {
         try {
-            const res = await axios.post("/api/course",values);
+            const res = await axios.post("/api/courses",values);
             router.push(`/teacher/course/${res.data.id}`)
+            toast.success("Course created");
 
 
         } catch {
